57575902ad71e42c99f361c2a8be4b96f2ed39e3
DhruvinXDev <dhruvinvaghasiya297@email.com>
2025-07-12 22:03:30 +0530

    add all file

---
+++ Video-87/path.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "/Users/vishh/Documents/Dhruvin"
+console.log(path.extname(myPath))
+
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/", "programs\\harry.txt"))
